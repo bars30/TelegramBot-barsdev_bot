93c7fbfbbab0beacaabfcedcbaa2b245e0218b57
anna3bars <154560345+anna3bars@users.noreply.github.com>
2024-10-02 15:30:35 +0400

    Starting

---
+++ index.js
@@ -0,0 +1,14 @@
+const TelegramApi = require('node-telegram-bot-api') //импортировать сюда этот пакет который 
+// мы устонавили npm i node-telegram-bot-api
+
+const token = '8171369003:AAGSA33hdyVSVX2sqN1zeO1_trFOpNoistU'
+
+const bot = new TelegramApi(token, {polling: true})
+
+
+
+
+
+
+
+
